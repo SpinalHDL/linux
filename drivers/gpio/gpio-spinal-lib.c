c9bdcfda4a35273f7b0097e27310880147142ef4
Charles Papon <charles.papon.90@gmail.com>
2019-06-02 13:35:41 +0200

    Add spinal-lib drivers
    Fix spi-spinal-lib driver
    
    Add gpio-spinal-lib driver
    
    Clean spi-spinal-lib
    
    gpio-spinal-lib rework and add interrupt support
    
    spi-spinal-lib improve speed and allow GPIO CS
    
    gpio-spinal-lib clear direction during probe
    
    irq-sifive-plic.c mask/unmask fix
    
    Fix gpio-spinal-lib interrupts
    
    fix spi-spinal-lib burst
    
    Fix spi-spinal-lib driver
    
    Fix spi-spinal-lib xfer
    
    spi-spinal-lib now use dts clock def
    
    spi_spinal_lib_speed now also update SS timings
    
    Add serial_spinal_lib support
    
    Add spinal_lib_mac
    
    spinal_lib_mac fix bug and improve speed
    
    spinal_lib_mac add rx stats support
    
    spinal_lib_mac now support interrupts
    
    Fix gpio-spinal-lib with 32 pin
    
    clean up spinal_lib_mac
    
    spinal-lib-dmasg added
    
    cleanup
    
    Add spinal_lib_audio_out
    
    spinal dma/audio cleanup
    
    spinal-lib-dmasg now support cyclic with unaligned period, segments are now builded on the fly instead of being static
    
    spinal_lib_audio_out extend frequancy range
    
    spinal-lib-dmasg remove debug printk
    
    gpio-spinal-lib.c unmask irq when irq_enable

---
+++ drivers/gpio/gpio-spinal-lib.c
@@ -0,0 +1,349 @@
+
+
+
+
+
+
+#include <linux/bitops.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/of_irq.h>
+#include <linux/gpio/driver.h>
+#include <linux/irqchip/chained_irq.h>
+#include <linux/init.h>
+#include <linux/of.h>
+#include <linux/pinctrl/consumer.h>
+#include <linux/platform_device.h>
+#include <linux/pm.h>
+#include <linux/slab.h>
+#include <linux/interrupt.h>
+#include <linux/spinlock.h>
+
+#define GPIO_INPUT		    0x0
+#define GPIO_OUTPUT			0x4
+#define GPIO_OUTPUT_ENABLE	0x8
+#define GPIO_RISE_IE		0x20
+#define GPIO_FALL_IE		0x24
+#define GPIO_HIGH_IE		0x28
+#define GPIO_LOW_IE			0x2C
+
+#define MAX_GPIO	32
+
+struct spinal_lib_gpio {
+	raw_spinlock_t		lock;
+	void __iomem		*base;
+	struct gpio_chip	gc;
+	unsigned long		enabled;
+	unsigned		trigger[MAX_GPIO];
+	unsigned int		irq_parent[MAX_GPIO];
+	struct spinal_lib_gpio	*self_ptr[MAX_GPIO];
+
+	u32 interrupt_mask;
+};
+
+static void spinal_lib_gpio_assign_bit(void __iomem *ptr, int offset, int value)
+{
+	// It's frustrating that we are not allowed to use the device atomics
+	// which are GUARANTEED to be supported by this device on RISC-V
+	u32 bit = BIT(offset), old = ioread32(ptr);
+	if (value)
+		iowrite32(old | bit, ptr);
+	else
+		iowrite32(old & ~bit, ptr);
+}
+
+static int spinal_lib_gpio_direction_input(struct gpio_chip *gc, unsigned offset)
+{
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	unsigned long flags;
+
+	if (offset >= gc->ngpio)
+		return -EINVAL;
+
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_OUTPUT_ENABLE, offset, 0);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+
+
+	return 0;
+}
+
+static int spinal_lib_gpio_direction_output(struct gpio_chip *gc, unsigned offset, int value)
+{
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	unsigned long flags;
+
+	if (offset >= gc->ngpio)
+		return -EINVAL;
+
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_OUTPUT, offset, value);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_OUTPUT_ENABLE,  offset, 1);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+
+	return 0;
+}
+
+static int spinal_lib_gpio_get_direction(struct gpio_chip *gc, unsigned offset)
+{
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+
+	if (offset >= gc->ngpio)
+		return -EINVAL;
+
+	return !(ioread32(chip->base + GPIO_OUTPUT_ENABLE) & BIT(offset));
+}
+
+static int spinal_lib_gpio_get_value(struct gpio_chip *gc, unsigned offset)
+{
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+
+	if (offset >= gc->ngpio)
+		return -EINVAL;
+
+	return !!(ioread32(chip->base + GPIO_INPUT) & BIT(offset));
+}
+
+static void spinal_lib_gpio_set_value(struct gpio_chip *gc, unsigned offset, int value)
+{
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	unsigned long flags;
+
+	if (offset >= gc->ngpio)
+		return;
+
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_OUTPUT, offset, value);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+}
+
+static void spinal_lib_gpio_set_ie(struct spinal_lib_gpio *chip, int offset)
+{
+	unsigned long flags;
+	unsigned trigger;
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	trigger = (chip->enabled & chip->interrupt_mask & BIT(offset)) ? chip->trigger[offset] : 0;
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_RISE_IE, offset, trigger & IRQ_TYPE_EDGE_RISING);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_FALL_IE, offset, trigger & IRQ_TYPE_EDGE_FALLING);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_HIGH_IE, offset, trigger & IRQ_TYPE_LEVEL_HIGH);
+	spinal_lib_gpio_assign_bit(chip->base + GPIO_LOW_IE,  offset, trigger & IRQ_TYPE_LEVEL_LOW);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+}
+
+static int spinal_lib_gpio_irq_set_type(struct irq_data *d, unsigned trigger)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	int offset = irqd_to_hwirq(d);
+
+	if (offset < 0 || offset >= gc->ngpio)
+		return -EINVAL;
+
+	chip->trigger[offset] = trigger;
+	spinal_lib_gpio_set_ie(chip, offset);
+	return 0;
+}
+
+//static void spinal_lib_gpio_update_interrupt_enable(struct spinal_lib_gpio *chip){
+//	iowrite32(chip->irq_rise_enable & chip->interrupt_mask, chip->base + GPIO_RISE_IE);
+//	iowrite32(chip->irq_fall_enable & chip->interrupt_mask, chip->base + GPIO_FALL_IE);
+//	iowrite32(chip->irq_high_enable & chip->interrupt_mask, chip->base + GPIO_HIGH_IE);
+//	iowrite32(chip->irq_low_enable & chip->interrupt_mask, chip->base + GPIO_LOW_IE);
+//}
+
+/* chained_irq_{enter,exit} already mask the parent */
+static void spinal_lib_gpio_irq_mask(struct irq_data *d) {
+	unsigned long flags;
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	int offset = irqd_to_hwirq(d) % MAX_GPIO; // must not fail
+
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	chip->interrupt_mask &= ~BIT(offset);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+
+	spinal_lib_gpio_set_ie(chip, offset);
+
+}
+static void spinal_lib_gpio_irq_unmask(struct irq_data *d) {
+	unsigned long flags;
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	int offset = irqd_to_hwirq(d) % MAX_GPIO; // must not fail
+
+	raw_spin_lock_irqsave(&chip->lock, flags);
+	chip->interrupt_mask |= BIT(offset);
+	raw_spin_unlock_irqrestore(&chip->lock, flags);
+
+	spinal_lib_gpio_set_ie(chip, offset);
+}
+
+static void spinal_lib_gpio_irq_enable(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	int offset = irqd_to_hwirq(d) % MAX_GPIO; // must not fail
+//	u32 bit = BIT(offset);
+
+	/* Switch to input */
+	spinal_lib_gpio_direction_input(gc, offset);
+
+	/* Clear any sticky pending interrupts */
+//	iowrite32(bit, chip->base + GPIO_RISE_IP);
+//	iowrite32(bit, chip->base + GPIO_FALL_IP);
+//	iowrite32(bit, chip->base + GPIO_HIGH_IP);
+//	iowrite32(bit, chip->base + GPIO_LOW_IP);
+
+	/* Enable interrupts */
+	assign_bit(offset, &chip->enabled, 1);
+	spinal_lib_gpio_set_ie(chip, offset);
+    spinal_lib_gpio_irq_unmask(d);
+}
+
+static void spinal_lib_gpio_irq_disable(struct irq_data *d)
+{
+	struct gpio_chip *gc = irq_data_get_irq_chip_data(d);
+	struct spinal_lib_gpio *chip = gpiochip_get_data(gc);
+	int offset = irqd_to_hwirq(d) % MAX_GPIO; // must not fail
+
+	assign_bit(offset, &chip->enabled, 0);
+	spinal_lib_gpio_set_ie(chip, offset);
+}
+
+static struct irq_chip spinal_lib_gpio_irqchip = {
+	.name		= "spinal_lib_gpio-gpio",
+	.irq_set_type	= spinal_lib_gpio_irq_set_type,
+	.irq_mask	= spinal_lib_gpio_irq_mask,
+	.irq_unmask	= spinal_lib_gpio_irq_unmask,
+	.irq_enable	= spinal_lib_gpio_irq_enable,
+	.irq_disable	= spinal_lib_gpio_irq_disable,
+};
+
+static void spinal_lib_gpio_irq_handler(struct irq_desc *desc)
+{
+	struct irq_chip *irqchip = irq_desc_get_chip(desc);
+	struct spinal_lib_gpio **self_ptr = irq_desc_get_handler_data(desc);
+	struct spinal_lib_gpio *chip = *self_ptr;
+	int offset = self_ptr - &chip->self_ptr[0];
+//	u32 bit = BIT(offset);
+
+	chained_irq_enter(irqchip, desc);
+
+	/* Re-arm the edge triggers so don't miss the next one */
+//	iowrite32(bit, chip->base + GPIO_RISE_IP);
+//	iowrite32(bit, chip->base + GPIO_FALL_IP);
+
+	generic_handle_irq(irq_find_mapping(chip->gc.irq.domain, offset));
+
+	/* Re-arm the level triggers after handling to prevent spurious refire */
+//	iowrite32(bit, chip->base + GPIO_HIGH_IP);
+//	iowrite32(bit, chip->base + GPIO_LOW_IP);
+
+	chained_irq_exit(irqchip, desc);
+}
+
+static int spinal_lib_gpio_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct device_node *node = pdev->dev.of_node;
+	struct spinal_lib_gpio *chip;
+	struct resource *res;
+	int gpio, irq, ret, ngpio;
+
+	chip = devm_kzalloc(dev, sizeof(*chip), GFP_KERNEL);
+	if (!chip) {
+		dev_err(dev, "out of memory\n");
+		return -ENOMEM;
+	}
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	chip->base = devm_ioremap_resource(dev, res);
+	if (IS_ERR(chip->base)) {
+		dev_err(dev, "failed to allocate device memory\n");
+		return PTR_ERR(chip->base);
+	}
+
+	if (of_property_read_u32(node, "ngpio", &ngpio))
+		ngpio = MAX_GPIO;
+
+
+	if (ngpio > MAX_GPIO) {
+		dev_err(dev, "too many GPIO\n");
+		return -EINVAL;
+	}
+
+	raw_spin_lock_init(&chip->lock);
+	chip->gc.direction_input = spinal_lib_gpio_direction_input;
+	chip->gc.direction_output = spinal_lib_gpio_direction_output;
+	chip->gc.get_direction = spinal_lib_gpio_get_direction;
+	chip->gc.get = spinal_lib_gpio_get_value;
+	chip->gc.set = spinal_lib_gpio_set_value;
+	chip->gc.base = -1;
+	chip->gc.ngpio = ngpio;
+	chip->gc.label = dev_name(dev);
+	chip->gc.parent = dev;
+	chip->gc.owner = THIS_MODULE;
+
+	ret = gpiochip_add_data(&chip->gc, chip);
+	if (ret)
+		return ret;
+
+	/* Disable all GPIO interrupts before enabling parent interrupts */
+	iowrite32(0, chip->base + GPIO_OUTPUT_ENABLE);
+	iowrite32(0, chip->base + GPIO_RISE_IE);
+	iowrite32(0, chip->base + GPIO_FALL_IE);
+	iowrite32(0, chip->base + GPIO_HIGH_IE);
+	iowrite32(0, chip->base + GPIO_LOW_IE);
+	chip->interrupt_mask = 0xFFFFFFFF;
+	chip->enabled = 0;
+
+
+
+	ret = gpiochip_irqchip_add(&chip->gc, &spinal_lib_gpio_irqchip, 0, handle_level_irq, IRQ_TYPE_NONE);
+	if (ret) {
+		dev_err(dev, "could not add irqchip\n");
+		gpiochip_remove(&chip->gc);
+		return ret;
+	}
+
+	chip->gc.irq.num_parents = ngpio;
+	chip->gc.irq.parents = &chip->irq_parent[0];
+	chip->gc.irq.map = &chip->irq_parent[0];
+
+	for (gpio = 0; gpio < ngpio; ++gpio) {
+		irq = platform_get_irq(pdev, gpio);
+		chip->irq_parent[gpio] = irq;
+		chip->self_ptr[gpio] = chip;
+		chip->trigger[gpio] = IRQ_TYPE_NONE;
+	}
+
+	for (gpio = 0; gpio < ngpio; ++gpio) {
+		irq = chip->irq_parent[gpio];
+		if(irq < 0) continue;
+//		if (gpio > 1) continue;
+
+		irq_set_chained_handler_and_data(irq, spinal_lib_gpio_irq_handler, &chip->self_ptr[gpio]);
+		irq_set_parent(irq_find_mapping(chip->gc.irq.domain, gpio), irq);
+	}
+
+	platform_set_drvdata(pdev, chip);
+	dev_info(dev, "Spinal lib GPIO chip registered %d GPIOs\n", ngpio);
+
+	return 0;
+}
+
+static const struct of_device_id spinal_lib_gpio_match[] = {
+	{
+		.compatible = "spinal-lib,gpio-1.0",
+	},
+	{ },
+};
+
+static struct platform_driver spinal_lib_gpio_driver = {
+	.probe		= spinal_lib_gpio_probe,
+	.driver = {
+		.name	= "spinal_lib_gpio",
+		.of_match_table = of_match_ptr(spinal_lib_gpio_match),
+	},
+};
+builtin_platform_driver(spinal_lib_gpio_driver)
---
+++ sound/spinal/spinal_lib_audio_out.c
@@ -0,0 +1,297 @@
+/*
+ * spinal_lib_audio_out.c
+ *
+ *  Created on: Sep 11, 2020
+ *      Author: rawrr
+ */
+
+
+
+#include <linux/io.h>
+#include <linux/of_platform.h>
+#include <linux/module.h>
+#include <linux/of_address.h>
+#include <linux/init.h>
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/dmaengine_pcm.h>
+#include <linux/clk.h>
+
+
+#define AUDIO_OUT_STATUS 0x10
+#define AUDIO_OUT_RATE 0x14
+
+struct spinal_lib_audio_out_device {
+    void __iomem *regs;
+    struct device *dev;
+    struct snd_card *card;
+    struct snd_pcm *pcm;
+    struct dma_chan* dma;
+    int position;
+    u32 hz;
+};
+
+static const struct snd_pcm_hardware sdma_pcm_hardware = {
+    .info          = SNDRV_PCM_INFO_MMAP |
+                     SNDRV_PCM_INFO_MMAP_VALID |
+                     SNDRV_PCM_INFO_NO_PERIOD_WAKEUP |
+                     SNDRV_PCM_INFO_INTERLEAVED,
+
+    .formats =      (SNDRV_PCM_FMTBIT_S16_LE),
+    .rates =        (SNDRV_PCM_RATE_CONTINUOUS | SNDRV_PCM_RATE_8000_192000),
+    .rate_min =     5500,
+    .rate_max =     192000,
+    .channels_min =     1,
+    .channels_max =     2,
+    .period_bytes_min   = 1 * 1024,
+    .period_bytes_max   = 64 * 1024,
+    .buffer_bytes_max   = 128 * 1024,
+    .periods_min        = 2,
+    .periods_max        = 255,
+    .fifo_size =        0,
+};
+
+
+
+static int spinal_lib_audio_out_open(struct snd_pcm_substream *substream)
+{
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    struct snd_pcm_runtime *runtime = substream->runtime;
+
+    //printk("spinal_lib_audio_out_open\n");
+
+    runtime->hw = sdma_pcm_hardware;
+    runtime->private_data = priv;
+    substream->private_data = priv;
+
+    return 0;
+}
+
+static int spinal_lib_audio_out_close(struct snd_pcm_substream *substream)
+{
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    //printk("spinal_lib_audio_out_close\n");
+    dmaengine_synchronize(priv->dma);
+    return 0;
+}
+
+static int spinal_lib_audio_out_hw_params(struct snd_pcm_substream *substream,
+                   struct snd_pcm_hw_params *hw_params)
+{
+    //printk("spinal_lib_audio_out_hw_params\n");
+
+    return snd_pcm_lib_malloc_pages(substream, params_buffer_bytes(hw_params));
+}
+
+static int spinal_lib_audio_out_hw_free(struct snd_pcm_substream *substream)
+{
+    //printk("spinal_lib_audio_out_hw_free\n");
+    return snd_pcm_lib_free_pages(substream);
+}
+
+static void spinal_lib_audio_out_complete(void *arg)
+{
+    struct snd_pcm_substream *substream = arg;
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    //printk("spinal_lib_audio_out_complete\n");
+
+    priv->position += snd_pcm_lib_period_bytes(substream);
+    if (priv->position >= snd_pcm_lib_buffer_bytes(substream))
+        priv->position = 0;
+
+    snd_pcm_period_elapsed(substream);
+}
+
+
+static int spinal_lib_audio_out_trigger(struct snd_pcm_substream *substream, int cmd)
+{
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    //printk("spinal_lib_audio_out_trigger\n");
+    switch (cmd) {
+    case SNDRV_PCM_TRIGGER_START:{
+        struct dma_async_tx_descriptor *desc;
+        unsigned long flags = DMA_CTRL_ACK;
+        dma_cookie_t cookie;
+
+        dev_info(priv->dev, "Start cha=%d rate=%d buf=%d per=%d\n",  (u32)substream->runtime->channels, (u32)substream->runtime->rate, (u32)substream->runtime->buffer_size, (u32)substream->runtime->period_size);
+        writel(priv->hz/substream->runtime->rate, priv->regs + AUDIO_OUT_RATE);
+        writel(substream->runtime->channels, priv->regs + AUDIO_OUT_STATUS);
+
+        priv->position = 0;
+
+        desc = dmaengine_prep_dma_cyclic(priv->dma,
+            substream->runtime->dma_addr,
+            snd_pcm_lib_buffer_bytes(substream),
+            snd_pcm_lib_period_bytes(substream), DMA_MEM_TO_DEV, flags);
+
+        if (!desc)
+            return -ENOMEM;
+
+        desc->callback = spinal_lib_audio_out_complete;
+        desc->callback_param = substream;
+        cookie = desc->tx_submit(desc);
+        if(dma_submit_error(cookie))
+            return cookie;
+
+        dma_async_issue_pending(priv->dma);
+
+        return 0;
+    }
+    case SNDRV_PCM_TRIGGER_STOP:
+        dmaengine_terminate_async(priv->dma);
+        writel(0, priv->regs + AUDIO_OUT_STATUS);
+        return 0;
+
+    case SNDRV_PCM_TRIGGER_RESUME:
+        return -EINVAL;
+    case SNDRV_PCM_TRIGGER_SUSPEND:
+        return -EINVAL;
+    }
+    return -EINVAL;
+}
+
+static int spinal_lib_audio_out_prepare(struct snd_pcm_substream *substream)
+{
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    //printk("spinal_lib_audio_out_prepare\n");
+
+    priv->position = 0;
+
+    if (IS_ERR(priv->dma)) {
+        return -EBUSY;
+    }
+
+    return 0;
+}
+
+static snd_pcm_uframes_t spinal_lib_audio_out_pointer(struct snd_pcm_substream *substream)
+{
+    struct spinal_lib_audio_out_device *priv = snd_pcm_substream_chip(substream);
+    //printk("spinal_lib_audio_out_pointer %d\n", priv->position);
+
+    return bytes_to_frames(substream->runtime, priv->position);
+}
+
+static struct snd_pcm_ops spinal_lib_audio_out_ops = {
+    .open =     spinal_lib_audio_out_open,
+    .close =    spinal_lib_audio_out_close,
+    .ioctl =    snd_pcm_lib_ioctl,
+    .hw_params =    spinal_lib_audio_out_hw_params,
+    .hw_free =  spinal_lib_audio_out_hw_free,
+    .prepare =  spinal_lib_audio_out_prepare,
+    .trigger =  spinal_lib_audio_out_trigger,
+    .pointer =  spinal_lib_audio_out_pointer,
+};
+
+static int spinal_lib_audio_out_register_pcm(struct spinal_lib_audio_out_device *priv){
+    struct snd_pcm_ops *ops;
+    int err;
+    //printk("spinal_lib_audio_out_register_pcm\n");
+    err = snd_pcm_new(priv->card, "Spinal lib audio", 0,
+                   1, 0, &priv->pcm);
+    if (err < 0)
+        return err;
+    ops = &spinal_lib_audio_out_ops;
+    snd_pcm_set_ops(priv->pcm, SNDRV_PCM_STREAM_PLAYBACK, ops);
+    snd_pcm_set_ops(priv->pcm, SNDRV_PCM_STREAM_CAPTURE, ops);
+    priv->pcm->private_data = priv;
+    priv->pcm->info_flags = 0;
+    strcpy(priv->pcm->name, "Spinal lib audio");
+    snd_pcm_lib_preallocate_pages_for_all(priv->pcm,
+            SNDRV_DMA_TYPE_DEV,
+        priv->dev,
+        64*1024, 64*1024);
+    return 0;
+}
+static int spinal_lib_audio_out_probe(struct platform_device *pdev)
+{
+    struct spinal_lib_audio_out_device *priv;
+    struct snd_card *card;
+    struct clk      *clk;
+    int err;
+
+    //printk("spinal_lib_audio_out_probe\n");
+
+    err = snd_card_new(&pdev->dev, 0, "Spinal lib audio out", THIS_MODULE,
+               sizeof(struct spinal_lib_audio_out_device), &card);
+    if (err < 0)
+        return err;
+
+    priv = card->private_data;
+    priv->card = card;
+    priv->dev = &pdev->dev;
+
+    priv->dma = dma_request_chan(priv->dev, "tx");
+
+
+    spinal_lib_audio_out_register_pcm(priv);
+
+
+    clk = devm_clk_get(&pdev->dev, NULL);
+    if (IS_ERR(clk)) {
+        dev_info(&pdev->dev, "No peripheral clock\n");
+        err = -1;
+        goto error;
+    }
+    priv->hz = clk_get_rate(clk);
+    if(!priv->hz){
+        dev_info(&pdev->dev, "Bad frequancy\n");
+        err = -1;
+        goto error;
+    }
+
+    /* Request and map I/O memory */
+    priv->regs = devm_ioremap_resource(&pdev->dev, platform_get_resource(pdev, IORESOURCE_MEM, 0));
+    if (IS_ERR(priv->regs))
+        return PTR_ERR(priv->regs);
+
+
+
+    err = snd_card_register(card);
+    if (err) {
+        goto error;
+    }
+
+
+    platform_set_drvdata(pdev, card);
+
+    dev_info(&pdev->dev, "Probe success\n");
+    return 0;
+
+error:
+    dev_info(&pdev->dev, "Probe failure :(\n");
+
+
+    return err;
+}
+
+
+static int spinal_lib_audio_out_remove(struct platform_device *pdev)
+{
+    //printk("spinal_lib_audio_out_remove\n");
+
+    return 0;
+}
+
+
+static const struct of_device_id spinal_lib_audio_out_of_ids[] = {
+    { .compatible = "spinal,lib-audio-out"},
+    {}
+};
+MODULE_DEVICE_TABLE(of, spinal_lib_audio_out_of_ids);
+
+static struct platform_driver spinal_lib_vdma_driver = {
+    .driver = {
+        .name = "spinal,lib-audio-out",
+        .of_match_table = spinal_lib_audio_out_of_ids,
+    },
+    .probe = spinal_lib_audio_out_probe,
+    .remove = spinal_lib_audio_out_remove,
+};
+
+module_platform_driver(spinal_lib_vdma_driver);
+
+MODULE_AUTHOR("Spinal");
+MODULE_DESCRIPTION("SpinalHDL audio_out driver");
+MODULE_LICENSE("GPL v2");
